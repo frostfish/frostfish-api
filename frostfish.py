07ffaced22a10a921f8be610ad748f50ca5e95be
Dmitry Bikulov <ksar@bikulov.org>
2018-03-24 15:46:51 +0300

    Fix

--- frostfish.py
+++ frostfish.py
@@ -2,8 +2,10 @@
 # Sample run: gunicorn frost-fish:app -b 0.0.0.0:5000 -D
 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 import json
 import os
